c77879163c337955646961f4155c0bb433443a60
XolarisDev <jon.vazquez0181@gmail.com>
2024-07-04 19:59:37 -0500

    Create AllTests.java

---
+++ src/AllTests.java
@@ -0,0 +1,13 @@
+package finalProject;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({
+	UserTest.class, MessageTest.class, ClientTest.class, 
+	LogTest.class, UserTest.class, ChatTest.class
+})
+public class AllTests {
+}
